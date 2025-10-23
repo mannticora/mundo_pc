0698641de8cd267168050de32be7285af9003c74
Ricardo Garcia Fentanes <ricardogarciafentanez@gmail.com>
2025-10-23 13:08:19 -0600

    se sobreescribre str y se comprueba programa

--- ClasesObjetos/mundo_pc/monitor.py
+++ ClasesObjetos/mundo_pc/monitor.py
@@ -6,5 +6,14 @@ class Monitor(DispositivoEntrada):
 
     def __init__(self, marca, tamano):
         Monitor.contador_monitores += 1
-        self. tamano = tamano
+        self.id_monitor = Monitor.contador_monitores
+        self.tamano = tamano
         super().__init__(marca)
+
+    def __str__(self):
+        return (f'id: {self}, Marca: {self.marca}'
+                f'Tamaño: {self.tamano}in')
+
+if __name__ == '__main__':
+    monitor1 = Monitor('Alien Ware', '10.5')
+    print(monitor1)
